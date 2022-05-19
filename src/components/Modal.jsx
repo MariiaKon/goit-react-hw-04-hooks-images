5ba3281fff4ce408f0c13e2715f29dcb547432e2
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-05-20 00:15:39 +0300

    added closing modal by pressing Esc

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,9 +2,21 @@ import { Overlay, ModalCss } from './styled/Modal.styled';
 import PropTypes from 'prop-types';
 
 export function Modal({ closeModal, image }) {
+  (() => {
+    window.addEventListener('keydown', e => {
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    });
+  })();
+
   return (
     <Overlay onClick={closeModal}>
-      <ModalCss>
+      <ModalCss
+        onClick={e => {
+          e.stopPropagation();
+        }}
+      >
         <img src={image.largeImageURL} alt={image.tags} />
       </ModalCss>
     </Overlay>
